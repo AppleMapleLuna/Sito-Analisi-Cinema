3c15641d36769b7db75ed1c06974925bd33c8bed
AppleMapleLuna <huchxi11@gmail.com>
2026-02-26 20:35:06 +0100

    dwasdwas

--- php.conndatabase.php
+++ php.conndatabase.php
@@ -1,18 +1,17 @@
 <?php
 // config.php
-$host = 'mysql-analisicinema.alwaysdata.net';
-$user = 'analisicinema';
-$password = 'CHDCciao1234';
-$dbname = 'analisicinema_database';
 
-// Creazione connessione
+$dotenv = parse_ini_file(__DIR__ . '/.env');
+
+$host = $dotenv['DB_HOST'];
+$user = $dotenv['DB_USER'];
+$password = $dotenv['DB_PASS'];
+$dbname = $dotenv['DB_NAME'];
+
 $conn = new mysqli($host, $user, $password, $dbname);
 
-// Controllo connessione
 if ($conn->connect_error) {
     die("Connessione fallita: " . $conn->connect_error);
 }
 
-// Imposta charset per gestire correttamente i caratteri
 $conn->set_charset("utf8mb4");
-?>
